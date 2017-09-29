1154b71d88af03eee998d50d6b8286eda229e45d
John Murphy <johnwmurphy@gmail.com>
2017-09-28 20:43:40 -0700

    Background, particles, and timer added

--- rocket.gd
+++ rocket.gd
@@ -4,7 +4,10 @@ var thrust = Vector2(0, 450)
 var torque = 20000
 
 func _ready():
-	pass
+	#pass
+	set_process(true)
+	set_contact_monitor(true)
+	set_max_contacts_reported(3)
 
 func _integrate_forces(state):
 	if Input.is_action_pressed("thrust"):
@@ -18,3 +21,6 @@ func _integrate_forces(state):
 		get_node("Particles2D").hide()
 	var t = Input.is_action_pressed("bear_right") - Input.is_action_pressed("bear_left")
 	set_applied_torque(torque * t)
+
+func _process(delta):
+	print(get_colliding_bodies())
