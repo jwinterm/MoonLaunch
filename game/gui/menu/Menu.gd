3d2ae7367aad68f85120a1e74bad0c77c0606530
John Murphy <johnwmurphy@gmail.com>
2017-10-07 21:38:01 -0700

    Got menu and explosion working

---
+++ game/gui/menu/Menu.gd
@@ -0,0 +1,18 @@
+extends VBoxContainer
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+		# Called every time the node is added to the scene.
+		# Initialization here
+		pass
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
+
+func _on_PlayButton_pressed():
+	get_tree().change_scene("res://game/levels/1/Level1.tscn")
--- game/rocket/rocket.gd
+++ game/rocket/rocket.gd
@@ -4,13 +4,20 @@ var thrust = Vector2(0, 450)
 var torque = 20000
 var shipoverlaps
 var footoverlaps
+var deathtimer 
 
 func _ready():
 	#pass
 	set_process(true)
 	set_contact_monitor(true)
 	set_max_contacts_reported(3)
-	get_node("Skull").hide()
+	get_node("SkullSprite").hide()
+	get_node("ExplosionSprite").hide()
+	deathtimer = Timer.new()
+	deathtimer.set_wait_time(3)
+	deathtimer.set_one_shot(true)
+	deathtimer.connect("timeout", self, "switchtomenu")
+	add_child(deathtimer)
 
 func _integrate_forces(state):
 	if Input.is_action_pressed("thrust"):
@@ -30,9 +37,24 @@ func _process(delta):
 	shipoverlaps = get_node("ShipArea").get_overlapping_bodies()
 	footoverlaps = get_node("FootArea").get_overlapping_bodies()
 	if (shipoverlaps.size() > 1):
-		get_node("Skull").show()
+		get_node("SkullSprite").show()
+		get_node("RocketSprite").hide()
+		death()
 	else:
-		get_node("Skull").hide()
+		get_node("SkullSprite").hide()
 	for i in footoverlaps:
 		if(i.get_name() == "Moon"):
-			get_node("Skull").show()
+			get_node("SkullSprite").show()
+
+func death():
+	set_process(false)
+	get_node("ExplosionSprite").show()
+	get_node("ExplosionSprite").get_node("AnimationPlayer").play("explode")
+	deathtimer.set_active(true)
+	deathtimer.start()
+	#while(deathtimer.get_wait_time() > 0.1):
+	print(deathtimer.get_wait_time())
+
+	
+func switchtomenu():
+	get_tree().change_scene("res://game/gui/menu/Menu.tscn")
